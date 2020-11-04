a986bd16fe92a1744034f62fb96df363deda5d1d
ragegit <maashwin115@gmail.com>
2020-11-04 11:14:18 +0530

    Update

--- RagePortal/RageKiosk/setup.ps1
+++ RagePortal/RageKiosk/setup.ps1
@@ -1,5 +1,5 @@
 ﻿$name = 'username='+ $env:UserName
 $cp='C:\Users\'+ $env:UserName + '\AppData\Local\'
-((Get-Content -path RageKiosk\RageKiosk\loginInfo\userInformation.ini -Raw) -replace 'username=.*', $name) | Set-Content -Path RageKiosk\RageKiosk\loginInfo\userInformation.ini
+((Get-Content -path RageKiosk\RageKiosk\loginInfo\userInformation.ini) -replace 'username=.*', $name) | Set-Content -Path RageKiosk\RageKiosk\loginInfo\userInformation.ini
 Copy-Item -Path "RageKiosk\RageKiosk" -Destination $cp -Recurse
 Start-Process -Wait -FilePath "RageKiosk\RageKiosk_setup_0.0.0.0_20201103_161055.exe" -ArgumentList "/S" -PassThru
